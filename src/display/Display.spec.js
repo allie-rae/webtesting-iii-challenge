e7d3c6ed03d12e964f8c9be7141b17276e2e834c
Allie Robinson <allierob93@gmail.com>
2019-12-16 17:52:43 -0600

    Added one simple test that is passing.

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,11 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
+
+import Display from './Display';
+
+test("Display renders without crashing", () => {
+    render(<Display />)
+})
+
