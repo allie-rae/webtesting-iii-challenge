7072c6287bb076e6ef5bd9a7ac92303428626b70
Allie Robinson <allierob93@gmail.com>
2019-12-17 22:07:28 -0600

    Finally understanding testing! Added 4 passing tests.

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -6,6 +6,32 @@ import '@testing-library/jest-dom/extend-expect'
 import Display from './Display';
 
 test("Display renders without crashing", () => {
-    render(<Display />)
+    const display = render(<Display />);
+    expect(display.asFragment()).toMatchSnapshot();
 })
 
+
+test("Displays Open / Unlocked", () => {
+    const display = render(<Display closed={false} locked={false} />);
+
+    expect(display.getByTestId('lockedDisplay')).toBeInTheDocument();
+    expect(display.getByTestId('closedDisplay')).toBeInTheDocument();
+
+})
+
+test("Displays locked & unlocked / red & green Classes", () => {
+    const display = render(<Display closed={false} locked={false} />);
+
+    const lockedDisplay = display.getByText(/unlocked/i);
+
+    lockedDisplay.classList.contains('green-led') && expect(lockedDisplay.textContent).toMatch(/unlocked/i);
+    lockedDisplay.classList.contains('red-led') && expect(lockedDisplay.textContent).toMatch(/locked/i);
+})
+
+test("Displays locked and red class", () => {
+    const display = render(<Display closed={true} locked={true} />);
+
+    expect(display.getByText(/locked/i));
+    expect(closedDisplay.classList.contains('red-led'));
+
+})
